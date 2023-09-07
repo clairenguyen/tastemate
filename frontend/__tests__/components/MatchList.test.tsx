b1e553bd31d16c1431eb9e45261b1b2a9375aad9
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2023-09-07 10:15:12 -0400

    Prettify the App
    - Add `components` directory
    - Remove default Next.js design and begin creating main page design

--- frontend/__tests__/components/MatchList.test.tsx
+++ frontend/__tests__/components/MatchList.test.tsx
@@ -1,6 +1,5 @@
 import {render,screen} from '@testing-library/react'
 import MatchList from "@/components/MatchList";
-import {beforeEach} from "@jest/globals";
 
 describe('MatchList', () => {
     it('renders the TasteMate h1', () => {
@@ -8,5 +7,15 @@ describe('MatchList', () => {
         const myElem = screen.getByText('TasteMate')
 
         expect(myElem).toBeInTheDocument()
+    })
+})
+
+    it('renders headers for "Like" lists ', () => {
+        render(<MatchList/>)
+        const restaurantsHeader = screen.getByText('Restaurants')
+        const peopleHeader = screen.getByText('People')
+
+        expect(restaurantsHeader).toBeInTheDocument()
+        expect(peopleHeader).toBeInTheDocument()
     })
 })
