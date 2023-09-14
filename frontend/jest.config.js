8cedfd96aeee598c94079b29d2e7913624b45090
Claire Nguyen <6858159+clairenguyen@users.noreply.github.com>
2023-09-14 10:24:56 -0700

    Add prettier package (#26)
    * Add prettier package
    
    * Run prettier for the first time on all frontend files
    
    * Update prettier script
    
    * Remove semi-colons
    
    * Update eslint config to place nicely with prettier

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,13 +1,13 @@
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-    dir: './',
+  dir: './',
 })
 
 const customJestConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleDirectories: ['node_modules', '<rootDir>/'],
-    testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
 }
 
 export default createJestConfig(customJestConfig)
