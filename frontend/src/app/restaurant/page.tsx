aabf0ff7b54bfc6b4be5a49daab9d28c1355d3ba
Claire Nguyen <6858159+clairenguyen@users.noreply.github.com>
2023-09-15 13:44:58 -0700

    Fetch and display restaurants  (no tests) (#30)
    * Update testing dependencies
      - Downgrade @testing-library/jest-dom due to latest version having known issues with typescript types
    
    * Fetch and display restaurant data on / restaurant route (no tests)
    
    * fetch restaurants use client (#38)
    
    * Update Restaurant type
    
    ---------
    
    Co-authored-by: Brian Sung <6445410+seoulection@users.noreply.github.com>

---
+++ frontend/src/app/restaurant/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { Restaurant } from '@/types'
+
+interface RestaurantProps {
+  restaurant: Restaurant
+}
+
+const RestaurantCard: React.FC<RestaurantProps> = ({ restaurant }) => {
+  return (
+    <>
+      <h1>{restaurant.name}</h1>
+      <div>{restaurant.address}</div>
+      <div>{restaurant.cuisine}</div>
+      <div>{restaurant.rating}</div>
+      <div>{restaurant.phone}</div>
+      <div>{restaurant.image_url}</div>
+      <div>{restaurant.website_url}</div>
+      <br />
+    </>
+  )
+}
+
+const RestaurantPage: React.FC<RestaurantProps> = () => {
+  const [error, setError] = useState()
+  const [restaurants, setRestaurants] = useState<Restaurant[]>()
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/restaurants/all')
+      .then((res) => res.json())
+      .then((res) => setRestaurants(res.data))
+      .catch((err) => setError(err.message))
+  }, [])
+
+  if (error) return <p>{error}</p>
+  if (!restaurants) return <p>Loading...</p>
+
+  return (
+    <div>
+      {restaurants.map((restaurant: Restaurant, key: number) => (
+        <RestaurantCard restaurant={restaurant} key={key} />
+      ))}
+    </div>
+  )
+}
+
+export default RestaurantPage
