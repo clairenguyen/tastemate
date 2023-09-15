db060a6d96863ad8537863af0fa3a601fa1f26e7
Karen Olson <karen.olson@mrdigitalpartners.com>
2023-09-15 16:27:22 -0500

    Displays restaurant images and adds minimal styling

--- frontend/src/app/restaurant/page.tsx
+++ frontend/src/app/restaurant/page.tsx
@@ -8,6 +8,27 @@ interface RestaurantProps {
   restaurant: Restaurant
 }
 
+const RestaurantCard: React.FC<RestaurantProps> = ({ restaurant }) => {
+  return (
+    <div style={{display: "flex"}}>
+      <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow max-w-md flex-col items-center" style={{color: "black", display: "flex"}}>
+          <div className="px-4 py-5 sm:px-6">
+              <a href={restaurant.website_url}>{restaurant.name}</a>
+          </div>
+          <div className="px-4 py-5 sm:p-6">
+            <p>{restaurant.address}</p>
+            <p>{restaurant.phone}</p>
+            <p>{restaurant.cuisine}</p>
+            <p>{restaurant.rating}</p>
+            <div className="h-48 w-96">
+              <img className="object-cover h-48 w-72" src={restaurant.image_url} />
+            </div>
+          </div>
+      </div>
+  </div>
+  )
+}
+
 const RestaurantPage: React.FC<RestaurantProps> = () => {
   const [error, setError] = useState()
   const [restaurants, setRestaurants] = useState<Restaurant[]>()
