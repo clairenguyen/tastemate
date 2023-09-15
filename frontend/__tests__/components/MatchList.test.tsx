bd5d28f6b14147353c0b6858bb343b2b9e95d85a
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2023-09-15 15:51:52 -0400

    Prettify the App (#22)
    * Prettify the App
    
    - Add `components` directory
    - Remove default Next.js design and begin creating main page design
    - Refactor postcss.config.js file to end in 'cjs' in order to get main page to appear on port 3000.

---
+++ frontend/__tests__/components/MatchList.test.tsx
@@ -0,0 +1,14 @@
+import {render,screen} from '@testing-library/react'
+import MatchList from "@/components/MatchList";
+import {beforeEach} from "@jest/globals";
+
+
+describe('MatchList', () => {
+
+    it('renders the TasteMate h1', () => {
+        render(<MatchList/>)
+        const myElem = screen.getByText('TasteMate')
+
+        expect(myElem).toBeInTheDocument()
+    })
+})
