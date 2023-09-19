75c4d721b9b0b926988d5c5b11bb6e980356032b
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-09-19 09:50:18 -0600

    Add cuisine filters

--- frontend/__tests__/components/FilterPanel.test.tsx
+++ frontend/__tests__/components/FilterPanel.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import FilterPanel from '../../src/components/FilterPanel';
-import FilterButton from '@/components/FilterButton';
 import '@testing-library/jest-dom';
 
 describe(FilterPanel, () => {
@@ -8,5 +7,41 @@ describe(FilterPanel, () => {
     render(<FilterPanel/>);
 
     expect(screen.getByText('Vegan')).toBeInTheDocument();
+  })
+
+  it('renders a FilterButton for each dietary restriction', () => {
+    const DIETARY_RESTRICTIONS = [
+      "Vegan",
+      "Vegetarian",
+      "Gluten Free",
+      "Kosher",
+    ]
+
+    render(<FilterPanel/>);
+
+    DIETARY_RESTRICTIONS.forEach((restriction) => {
+      expect(screen.getByText(restriction)).toBeInTheDocument();
+    })
+  })
+
+  it('renders a FilterButton for each cuisine', () => {
+    const CUISINES = [
+      "American",
+      "Italian",
+      "Fast Food",
+      "Pizza",
+      "Mexican",
+      "Asian",
+      "Tacos",
+      "Mediterranean",
+      "Sushi",
+    ]
+
+    render(<FilterPanel/>);
+
+    CUISINES.forEach((cuisine) => {
+      expect(screen.getByText(cuisine)).toBeInTheDocument();
+    }
+    )
   })
 })
--- frontend/src/components/FilterPanel.tsx
+++ frontend/src/components/FilterPanel.tsx
@@ -1,15 +1,37 @@
 import FilterButton from "./FilterButton";
 
 const DIETARY_RESTRICTIONS = [
-  "Vegan"
+  "Vegan",
+  "Vegetarian",
+  "Gluten Free",
+  "Kosher",
+]
+
+const CUISINES = [
+  "American",
+  "Italian",
+  "Fast Food",
+  "Pizza",
+  "Mexican",
+  "Asian",
+  "Tacos",
+  "Mediterranean",
+  "Sushi",
 ]
 
 export default function FilterPanel() {
   return (
     <>
-      {DIETARY_RESTRICTIONS.map((restriction) => (
-        <FilterButton key={restriction} filterLabel={restriction}/>
-      ))}
+      <div>
+        {DIETARY_RESTRICTIONS.map((restriction) => (
+          <FilterButton key={restriction} filterLabel={restriction}/>
+        ))}
+      </div>
+      <div>
+        {CUISINES.map((cuisine) => (
+          <FilterButton key={cuisine} filterLabel={cuisine}/>
+        ))}
+      </div>
     </>
   )
 }
