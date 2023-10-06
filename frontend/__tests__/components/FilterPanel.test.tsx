7354420cf8fccbbeeccb8159248cade0163e3431
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-10-06 10:51:28 -0600

    Add tests for filters rendering to FilterPanel

--- frontend/__tests__/components/FilterPanel.test.tsx
+++ frontend/__tests__/components/FilterPanel.test.tsx
@@ -7,4 +7,63 @@ describe(FilterPanel, () => {
     render(<FilterPanel />)
     expect(screen.getByText('Your Preferences')).toBeInTheDocument()
   })
+
+  it('renders a FilterCategory for each Dietary Restrictions filter', () => {
+    const DIETARY_RESTRICTIONS = [
+      'Vegan',
+      'Vegetarian',
+      'Gluten Free',
+      'Kosher',
+    ]
+
+    render(<FilterPanel />)
+
+    expect(screen.getByText('Dietary Restrictions')).toBeInTheDocument()
+    DIETARY_RESTRICTIONS.forEach((restriction) => {
+      expect(screen.getByText(restriction)).toBeInTheDocument()
+    })
+  })
+
+  it('renders a FilterCategory for each Cuisines filter', () => {
+    const CUISINES = [
+      'American',
+      'Italian',
+      'Fast Food',
+      'Pizza',
+      'Mexican',
+      'Asian',
+      'Tacos',
+      'Mediterranean',
+      'Sushi',
+    ]
+
+    render(<FilterPanel />)
+
+    expect(screen.getByText('Cuisines')).toBeInTheDocument()
+    CUISINES.forEach((cuisine) => {
+      expect(screen.getByText(cuisine)).toBeInTheDocument()
+    })
+  })
+
+  it('renders a FilterCategory for each Budget filter', () => {
+    const BUDGET = ['$', '$$', '$$$', '$$$$']
+
+    render(<FilterPanel />)
+
+    expect(screen.getByText('Budget')).toBeInTheDocument()
+    BUDGET.forEach((budget) => {
+      expect(screen.getByText(budget)).toBeInTheDocument()
+    })
+  })
+
+  it('renders a FilterCategory for each Ratings filter', () => {
+    const RATINGS = ['2+', '3+', '4+', '5+']
+
+    render(<FilterPanel />)
+
+    expect(screen.getByText('Ratings')).toBeInTheDocument()
+    RATINGS.forEach((rating) => {
+      expect(screen.getByText(rating)).toBeInTheDocument()
+    })
+  })
 })
