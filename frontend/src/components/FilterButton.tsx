cddefc7fc0ddaaac9543d92013aac264400c2602
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-10-13 09:06:14 -0600

    Increase button padding

--- frontend/src/components/FilterButton.tsx
+++ frontend/src/components/FilterButton.tsx
@@ -2,7 +2,7 @@ export default function FilterButton({ filterLabel }: { filterLabel: string }) {
   return (
     <>
       <button
-        className="text-white bg-blue-600 border rounded-full p-1"
+        className="text-white bg-blue-600 border rounded-full p-2"
         id={filterLabel}
       >
         {filterLabel}
