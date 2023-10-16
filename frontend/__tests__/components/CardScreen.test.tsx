e2ad47b0b19c68d5a68cd1c038ed2b11ab6644ac
Rian Pickell <46903319+rianp@users.noreply.github.com>
2023-10-16 15:47:59 -0700

    add restaurant filter button (#47)
    * add restaurant filter button

--- frontend/__tests__/components/CardScreen.test.tsx
+++ frontend/__tests__/components/CardScreen.test.tsx
@@ -1,5 +1,6 @@
 import { act, render, screen } from '@testing-library/react'
 import CardScreen from '@/components/CardScreen'
+import userEvent from '@testing-library/user-event'
 
 describe('CardScreen', () => {
   afterEach(() => {
@@ -50,5 +51,20 @@ describe('CardScreen', () => {
     })
 
     expect(screen.getByText(/failed to fetch/i)).toBeVisible()
+  })
+
+  test('clicking on restaurant filter button opens filter options', async () => {
+    window.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ data: [] }),
+    })
+
+    await act(() => {
+      render(<CardScreen />)
+    })
+
+    expect(screen.queryByText('Filters')).not.toBeInTheDocument()
+    
+    await userEvent.click(screen.getByRole('button', { name: 'Filter' }))
+    expect(screen.getByText('Filters')).toBeInTheDocument()
   })
 })
