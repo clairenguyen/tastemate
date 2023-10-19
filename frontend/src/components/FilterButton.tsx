2156e4ecc92cf5540196a46be2a6c80edcd820e8
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-10-19 07:34:14 -0600

    refactor: remove unnecessary fragment

--- frontend/src/components/FilterButton.tsx
+++ frontend/src/components/FilterButton.tsx
@@ -4,13 +4,11 @@ interface FilterButtonProps {
 
 export default function FilterButton({ label }: FilterButtonProps) {
   return (
-    <>
-      <button
-        className="text-white bg-blue-600 border rounded-full p-2"
-        id={label}
-      >
-        {label}
-      </button>
-    </>
+    <button
+      className="text-white bg-blue-600 border rounded-full p-2"
+      id={label}
+    >
+      {label}
+    </button>
   )
 }
