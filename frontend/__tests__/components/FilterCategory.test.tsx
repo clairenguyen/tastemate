bd4c9be323f95d3f07269e7e4ba496a100385a6a
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-10-19 07:36:14 -0600

    refactor: rename variable to lowercase

--- frontend/__tests__/components/FilterCategory.test.tsx
+++ frontend/__tests__/components/FilterCategory.test.tsx
@@ -5,12 +5,12 @@ import FilterCategory from '../../src/components/FilterCategory'
 
 describe(FilterCategory, () => {
   it('displays a filter button for each option', () => {
-    const OPTIONS = ['Option 1', 'Option 2', 'Option 3']
+    const options = ['Option 1', 'Option 2', 'Option 3']
 
-    render(<FilterCategory title="Test Category" options={OPTIONS} />)
+    render(<FilterCategory title="Test Category" options={options} />)
 
     expect(screen.getByText('Test Category')).toBeVisible()
-    OPTIONS.forEach((option) => {
+    options.forEach((option) => {
       expect(screen.getByText(option)).toBeVisible()
     })
   })
